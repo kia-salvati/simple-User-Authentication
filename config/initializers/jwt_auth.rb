d0423137e560d5145e22904b2500c2b3baa7beb7
kia <kiyanosh.s@gmail.com>
2023-03-12 20:06:01 +0330

    add expiration to JWT encodeing

--- config/initializers/jwt_auth.rb
+++ config/initializers/jwt_auth.rb
@@ -2,10 +2,14 @@ require 'jwt'
 
 class JwtAuth
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, 
+      exp: 24.hours.from_now.to_i, 
+      Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+    JWT.decode(token, 
+      Rails.application.secrets.secret_key_base, 
+      true, algorithm: 'HS256')
   end
 end
