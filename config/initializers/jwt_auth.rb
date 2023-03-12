aad9b961a7ba19bded9c0a74b737cdbbd002b3ac
kia <kiyanosh.s@gmail.com>
2023-03-12 23:01:09 +0330

    fixed samll white spaces

--- config/initializers/jwt_auth.rb
+++ config/initializers/jwt_auth.rb
@@ -2,13 +2,14 @@ require 'jwt'
 
 class JwtAuth
   def self.encode(payload)
-    JWT.encode(payload, 
-      exp: 24.hours.from_now.to_i, 
+    JWT.encode(
+      payload,
       Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    JWT.decode(token, 
+    JWT.decode(
+      token, 
       Rails.application.secrets.secret_key_base, 
       true, algorithm: 'HS256')
   end
