4d17b209e648155a80a088a536294b6eeed5e1b6
kia <kiyanosh.s@gmail.com>
2023-03-12 20:17:13 +0330

    update devise routes for user; add scope for api and changed the sign_in and sign_out to login and logout; define custom controllers for device

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :user
+  scope :api, defaults: { format: json } do 
+    devise_for :users, cotrollers: { 
+                sessions: 'sessions',
+                registrations: 'registrations'},
+                path_names: { sign_in: :login,
+                              sing_out: :logout }
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
