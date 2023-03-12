424609b4e3346f685fa368b10e42fac088b7ce47
kia <kiyanosh.s@gmail.com>
2023-03-12 22:58:11 +0330

    updated tabel after adding reset password token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_12_152012) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_12_185517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_12_152012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
