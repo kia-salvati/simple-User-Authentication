eac4de2b1a748c5d0d6a9d1d7e21ff9b45eb3b39
kia <kiyanosh.s@gmail.com>
2023-03-13 18:15:45 +0330

    modified routes fot registration and delete json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  scope :api, defaults: { format: :json } do 
+  scope :api do 
     devise_for :users, cotrollers: { 
                 sessions: 'sessions',
                 registrations: 'registrations'},
-                path_names: { sign_in: :login,
-                              sing_out: :logout }
+                path: '',
+                path_names: {
+                              sign_in: 'login',
+                              sing_out: 'logout',
+                              registration: 'signup'
+                             }
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
