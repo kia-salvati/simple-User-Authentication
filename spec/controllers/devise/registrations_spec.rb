65261a32f67eb0b08e676a98a5ff4684e3238f59
kia <kiyanosh.s@gmail.com>
2023-03-13 18:14:42 +0330

    modified Registrations and Sessions controller in rspec, neither working

--- spec/controllers/devise/registrations_spec.rb
+++ spec/controllers/devise/registrations_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Registrations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe RegistrationsController, type: :request do
+  describe "#Create" do
+    context 'valid requests' do
+      it 'create a new user' do
+        attributes = FactoryBot.attributes_for(:user)
+        attributes[:password_confirmation] = attributes[:password]
+        binding.pry
+        post "/api/signup", params: { 
+          user: attributes
+        }
+        json = json_parser
+        
+        expect { response }.to have_http_status(:created) #201
+        expect(json).to include(attributes[:username])
+      end
+    end
+  end
+
+  private
+
+  def json_parser
+    JSON.parse(response.body)
   end
 end
