18e67153b936795b6e9e4357450e3b339db1526c
kia <kiyanosh.s@gmail.com>
2023-03-13 20:44:23 +0330

    fixed routes for signup, login and logout

--- spec/routes/user_spec.rb
+++ spec/routes/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :routing do
   it 'Create user' do
-    expect(post: "/api/signup").to route_to(controller: 'registrations', action: 'create')
+    expect(post: "/api/signup").to route_to(controller: 'users/registrations', action: 'create')
   end
 
   it 'Create jwt token' do
-    expect(post: "/api/login").to route_to(controller: 'session', action: 'create')
+    expect(post: "/api/login").to route_to(controller: 'users/sessions', action: 'create')
   end
 
   it 'Delete jwt token' do 
-    expect(delete: '/api/sign_out').to route_to(controller: 'session', action: 'destroy')
+    expect(delete: '/api/logout').to route_to(controller: 'users/sessions', action: 'destroy')
   end
 end
