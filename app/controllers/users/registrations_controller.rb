77d8761543edc92ee6c03409edf61a88e89e7c5a
kia <kiyanosh.s@gmail.com>
2023-03-13 21:26:23 +0330

    add limets to the data send back to the user to only email and jti

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :jsonapi
 
   def create
-    user = Users::RegistrationService.run(sign_up_params)
+    user = Users::RegistrationService.run(params: sign_up_params)
     
     if user.valid?
-      render jsonapi: user.result, include: %w(email jti),status: :created #201
+      render jsonapi: user.result, fields: { user: %w(email jti) }, status: :created #201
     else
       render jsonapi_errors:  { detail: user.errors.messages }, status: :not_acceptable #406
     end
