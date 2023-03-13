7834bc5b2f63f54cf72b5b02c4af85e76e69c8f1
kia <kiyanosh.s@gmail.com>
2023-03-14 01:20:15 +0330

    modified create session servies to check for validation of inputs

--- app/services/users/create_session_service.rb
+++ app/services/users/create_session_service.rb
@@ -6,20 +6,27 @@ module Users
 
     def execute
       find_user
-
+      
       user
     end
 
     private
    
     def find_user
+      
       @user = User.find_by(email: params[:email])
 
-      user.valid_password?(params[:password])
-        
-      user.jti = user.generate_jwt if user.valid?
-
-      errors.merge!(user.errors) if user.errors.present?
+      if user.present?
+        if user.valid_password?(params[:password])
+          user.jti = user.generate_jwt
+        else
+          @user = User.new
+          user.errors.add(:user, "invalid password or email")
+        end
+      else
+        @user = User.new
+        user.errors.add(:user, "invalid password or email")
+      end
     end
   end
 end
