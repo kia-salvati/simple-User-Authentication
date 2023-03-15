ebbe318b6f590745d05b5477a0c32ee39213ac04
kia <kiyanosh.s@gmail.com>
2023-03-15 22:46:49 +0330

    add authenticate_user! for decoding any create request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_user!, only: %i(create) 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:signup, keys: [:username])
+  end
+
+  def authenticate_user!
+    token = request.params[:user]
+    request.params[:user] = JwtAuth.decode(token)
   end
 end
