7a286d44309f9fd8fc79024a011ab3d847341b93
kia <kiyanosh.s@gmail.com>
2023-03-15 23:19:06 +0330

    now authenticate_user! only applys to create respose

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  before_action :authenticate_user!, only: %i(create) 
+  before_action :authenticate_user!, only: %i(create)
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
